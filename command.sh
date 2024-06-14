5a814a950b17426c5e1ad850443dcab3de51876a
huzhengyang <Lyndon.Hu9@gmail.com>
2024-06-14 16:19:16 +0800

    backup

---
+++ command.sh
@@ -0,0 +1,16 @@
+#wasabi==0.10.1
+#watchfiles==0.21.0
+#weasyprint==53.3
+#websockets==10.3
+#werkzeug==3.0.1
+#wordcloud==1.9.2
+#wsproto==1.2.0
+#xarray==2024.2.0
+#xarray-einstats==0.7.0
+#xgboost==1.4.2
+# xlsxwriter==3.2.0
+# xml-python==0.4.3
+# yarl==1.9.4
+
+
+docker run -it --rm --name kmp -u root --entrypoint /bin/bash elyra/kernel-py:3.2.3
